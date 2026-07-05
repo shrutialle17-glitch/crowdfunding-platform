81abebf7b1dab1cbcf124b98c295de788c824bfe
shrutialle17-glitch <shrutialle17@gmail.com>
2026-07-06 04:37:03 +0530

    Fixed

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Database Connection Failed:", error.message);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
