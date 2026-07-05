475bcc31b18629f109cefc0f097f338e9d400490
shrutialle17-glitch <shrutialle17@gmail.com>
2026-07-06 05:09:38 +0530

    Ready for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -29,8 +29,11 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+    origin: [
+        "http://localhost:5173",
+        process.env.CLIENT_URL
+    ],
+    credentials: true
 }));
 
 // Rate Limiter
